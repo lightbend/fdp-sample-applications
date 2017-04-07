9d87ee381ba8619e4fc8b918c4e2e693bf024ec9
Dean Wampler <dean@concurrentthought.com>
2017-04-07 16:29:04 -0500

    Refined and standardized the release process.

--- release/fdp-package.sh
+++ release/fdp-package.sh
@@ -6,27 +6,54 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
 ROOT_DIR="${DIR}/.."
 
 CONTENT_FILE=${DIR}/content.txt
-VERSION_FILE=${DIR}/version.txt
-
 CONTENT=$(cat $CONTENT_FILE)
-VERSION=$(cat $VERSION_FILE)
 
-OUTPUT_FILE=fdp-system-tests-${VERSION}.tar.gz
-
-function show_help {
+function usage {
   cat<< EOF
-  This script packages the system tests project into $OUTPUT_FILE in the project root directory.
-  Usage: $SCRIPT  [OPTIONS]
-  (There are no command line options)
+  This script packages the sample applications into fdp-sample-apps-<version>.tar.gz
+  in the project root directory.
+  Usage: $SCRIPT VERSION [-h | --help]
+
+  VERSION       E.g., 0.3.0. Required
+  -h | --help   This message.
 EOF
 }
 
-case $1 in
-  -h|--help)
-    show_help
-    exit 0
-   ;;
-esac
+while [ $# -ne 0 ]
+do
+  case $1 in
+    -h|--help)
+      usage
+      exit 0
+      ;;
+    -*)
+      echo "$0: ERROR: Unrecognized argument $1"
+      usage
+      exit 1
+      ;;
+    *)
+      VERSION=$1
+      ;;
+  esac
+  shift
+done
 
-tar -C ${ROOT_DIR} -cf ${ROOT_DIR}/${OUTPUT_FILE} ${CONTENT}
+if [[ -z "$VERSION" ]]
+then
+  echo "$0: ERROR: The version argument is required."
+  usage
+  exit 1
+fi
+
+OUTPUT_FILE=fdp-sample-apps-${VERSION}.tar.gz
+
+staging=$DIR/staging
+rm -rf $staging
+mkdir -p $staging
+for f in ${CONTENT}; do cp -r $f $staging/$f; done
+cd $staging
+echo running: tar -czf ${OUTPUT_FILE} ${CONTENT}
+tar -czf ${OUTPUT_FILE} ${CONTENT}
+
+for f in ${CONTENT}; do rm -rf $f; done
 
