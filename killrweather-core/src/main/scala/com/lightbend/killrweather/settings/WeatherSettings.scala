f6ba6b7a4533f53d003eee7dd17e43117e4089ad
seglo <seglo@randonom.com>
2017-09-11 14:33:34 -0400

    Use new Kafka service by default. Revert to HDFS name node 0.

--- killrweather-core/src/main/scala/com/lightbend/killrweather/settings/WeatherSettings.scala
+++ killrweather-core/src/main/scala/com/lightbend/killrweather/settings/WeatherSettings.scala
@@ -43,7 +43,7 @@ final class WeatherSettings extends Serializable {
 
   val localAddress = "localhost" //InetAddress.getLocalHost.getHostAddress
 
-  val kafkaBrokers = /*"localhost:9092"*/ "broker.confluent-kafka.l4lb.thisdcos.directory:9092" // for dc/os
+  val kafkaBrokers = /*"localhost:9092"*/ "broker.kafka.l4lb.thisdcos.directory:9092" // for dc/os
 
   val SparkMaster = "local[*]"
 
@@ -51,7 +51,7 @@ final class WeatherSettings extends Serializable {
 
   val SparkStreamingBatchInterval = 5000L
 
-  val SparkCheckpointDir = "hdfs://name-1-node.hdfs.mesos:9001/cpt/"
+  val SparkCheckpointDir = "hdfs://name-0-node.hdfs.mesos:9001/cpt/"
 
   val CassandraHosts = /*localAddress*/ "node.cassandra.l4lb.thisdcos.directory" // This will work only on cluster
 
