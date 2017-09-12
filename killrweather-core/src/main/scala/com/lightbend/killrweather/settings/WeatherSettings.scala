5f87250a398bd354b5836ebcaf3c606a09716cca
seglo <seglo@randonom.com>
2017-09-12 14:12:19 -0400

    Use hadoop configuration for hdfs checkpointing

--- killrweather-core/src/main/scala/com/lightbend/killrweather/settings/WeatherSettings.scala
+++ killrweather-core/src/main/scala/com/lightbend/killrweather/settings/WeatherSettings.scala
@@ -51,7 +51,7 @@ final class WeatherSettings extends Serializable {
 
   val SparkStreamingBatchInterval = 5000L
 
-  val SparkCheckpointDir = "hdfs://name-0-node.hdfs.mesos:9001/cpt/"
+  val SparkCheckpointDir = "hdfs://hdfs/cpt/"
 
   val CassandraHosts = /*localAddress*/ "node.cassandra.l4lb.thisdcos.directory" // This will work only on cluster
 
