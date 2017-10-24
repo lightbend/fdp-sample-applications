c8026ef6106ba4e94f2ccc3b8cf54bddb58be1b1
Dean Wampler <dean@concurrentthought.com>
2017-10-24 10:55:16 -0500

    Updated release process

--- release/fdp-package.sh
+++ release/fdp-package.sh
@@ -45,7 +45,7 @@ then
   exit 1
 fi
 
-OUTPUT_FILE_ROOT=fdp-akka-kafka-streams-modelServer-${VERSION}
+OUTPUT_FILE_ROOT=fdp-akka-kafka-streams-model-server-${VERSION}
 OUTPUT_FILE=${OUTPUT_FILE_ROOT}.zip
 
 staging=$DIR/staging
@@ -55,7 +55,13 @@ mkdir -p $staging
 mkdir -p $staging/$OUTPUT_FILE_ROOT
 for f in ${CONTENT}; do cp -r ${ROOT_DIR}/$f $staging/$OUTPUT_FILE_ROOT/$f; done
 cd $staging
+
+# Remove files and directories that shouldn't be in the distribution:
+find ${OUTPUT_FILE_ROOT} \( -name whitesource.sbt -o -name WhitesourceLicensePlugin.scala \) -exec rm {} \;
+find ${OUTPUT_FILE_ROOT} -type d | egrep 'project/(project|target)$' | while read d; do rm -rf "$d"; done
+find ${OUTPUT_FILE_ROOT} -type d | egrep 'target$' | while read d; do rm -rf "$d"; done
+
 echo running: zip -r ${OUTPUT_FILE} ${OUTPUT_FILE_ROOT}
 zip -r ${OUTPUT_FILE} ${OUTPUT_FILE_ROOT}
 
-rm -rf ${OUTPUT_FILE_ROOT}
+# rm -rf ${OUTPUT_FILE_ROOT}
