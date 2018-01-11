2a1ce191e465cd7913aed3d6d9e3ab01f97274b1
Debasish Ghosh <ghosh.debasish@gmail.com>
2018-01-11 17:17:23 +0530

    Upgrades version of packages, rewrite kstream apps and nwintrusion (part) using kafka-streams-scala and kafka-streams-query

---
+++ kstream/source/core/example-proc/src/main/scala/com/lightbend/fdp/sample/kstream/processor/BFStoreBuilder.scala
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2018 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package com.lightbend.fdp.sample.kstream
+package processor
+
+import org.apache.kafka.streams.state.StoreBuilder
+import com.twitter.algebird.Hash128
+
+class BFStoreBuilder[T: Hash128](val storeSupplier: BFStoreSupplier[T]) extends StoreBuilder[BFStore[T]] {
+
+    override def name(): String = storeSupplier.name
+
+    override def build(): BFStore[T] = storeSupplier.get()
+
+    override def logConfig: java.util.Map[String, String] = storeSupplier.logConfig
+
+    override def loggingEnabled(): Boolean = storeSupplier.loggingEnabled
+
+    override def withCachingEnabled(): BFStoreBuilder[T] = this
+
+    override def withLoggingDisabled(): BFStoreBuilder[T] = {
+      storeSupplier.logConfig.clear()
+      this
+    }
+
+    override def withLoggingEnabled(config: java.util.Map[String, String]): BFStoreBuilder[T] = { 
+      new BFStoreBuilder[T](
+        new BFStoreSupplier(
+          storeSupplier.name, 
+          storeSupplier.serde, 
+          storeSupplier.loggingEnabled, 
+          config
+        )
+      )
+    }
+}
