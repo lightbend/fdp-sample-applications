7c1d8f530b6884308f7af523f76c444166c2c923
Gerard <gerard.maas@lightbend.com>
2018-03-07 16:56:09 +0100

    improves error handling on embedded kafka startup

--- akkaserver/src/main/scala/com/lightbend/modelServer/kafka/EmbeddedSingleNodeKafkaCluster.scala
+++ akkaserver/src/main/scala/com/lightbend/modelServer/kafka/EmbeddedSingleNodeKafkaCluster.scala
@@ -6,37 +6,57 @@ import java.util.Properties
 
 import kafka.server.KafkaConfig
 
+import scala.util.{ Failure, Try }
+
 object EmbeddedSingleNodeKafkaCluster {
   private val log = LoggerFactory.getLogger("EmbeddedSingleNodeKafkaCluster")
   private val DEFAULT_BROKER_PORT = 9092
-  private var zookeeper = null.asInstanceOf[TestingServer]
-  private var broker = null.asInstanceOf[KafkaEmbedded]
+  private var zookeeper: Option[TestingServer] = None
+  private var broker: Option[KafkaEmbedded] = None
 
   /**
    * Creates and starts a Kafka cluster.
    */
   def start(): Unit = {
-    // Ensure no duplcates
-    if (broker != null)
-      EmbeddedSingleNodeKafkaCluster.log.debug("Embedded Kafka cluster is already running... returning")
-    else {
-      val brokerConfig = new Properties
-      EmbeddedSingleNodeKafkaCluster.log.debug("Initiating embedded Kafka cluster startup")
-      EmbeddedSingleNodeKafkaCluster.log.debug("Starting a ZooKeeper instance")
-      try
-        zookeeper = new TestingServer(2181)
-      catch {
-        case e: Exception =>
-          // TODO Auto-generated catch block
-          e.printStackTrace()
-      }
-      EmbeddedSingleNodeKafkaCluster.log.debug(s"ZooKeeper instance is running at $zKConnectString")
-      brokerConfig.put(KafkaConfig.ZkConnectProp, zKConnectString)
-      brokerConfig.put(KafkaConfig.PortProp, DEFAULT_BROKER_PORT.toString)
-      EmbeddedSingleNodeKafkaCluster.log.debug(s"Starting a Kafka instance on port ${brokerConfig.getProperty(KafkaConfig.PortProp)} ...")
-      broker = new KafkaEmbedded(brokerConfig)
-      broker.start()
-      EmbeddedSingleNodeKafkaCluster.log.debug(s"Kafka instance is running at ${broker.brokerList}, connected to ZooKeeper at ${broker.zookeeperConnect}")
+    broker = broker match {
+      case bkr @ Some(b) =>
+        log.debug("Broker already started")
+        bkr
+      case None =>
+        val server = startZookeeper().flatMap { zk =>
+          log.debug("Initiating embedded Kafka cluster startup")
+          val brokerConfig = {
+            val p = new Properties
+            p.put(KafkaConfig.ZkConnectProp, zKConnectString)
+            p.put(KafkaConfig.PortProp, DEFAULT_BROKER_PORT.toString)
+            p
+          }
+          log.debug(s"Starting a Kafka instance on port ${brokerConfig.getProperty(KafkaConfig.PortProp)} ...")
+          Try {
+            val _broker = new KafkaEmbedded(brokerConfig)
+            _broker.start()
+            log.debug(s"Kafka instance is running at ${_broker.brokerList}, connected to ZooKeeper at ${_broker.zookeeperConnect}")
+            _broker
+          }.recoverWith {
+            case ex: Throwable =>
+              log.error(s"Unable to initialize Kafka instance. Reason: ${ex.getMessage}")
+              Failure(ex)
+          }
+        }
+        Some(server.get) // surface exceptions if any
+    }
+  }
+
+  def startZookeeper(): Try[TestingServer] = {
+    Try {
+      val zk = new TestingServer(2181)
+      log.debug("Starting a ZooKeeper instance")
+      log.debug(s"ZooKeeper instance is running at ${zk.getConnectString}")
+      zk
+    }.recoverWith {
+      case ex: Throwable =>
+        log.error(s"Unable to initialize ZooKeeper instance. Reason: ${ex.getMessage}")
+        Failure(ex)
     }
   }
 
@@ -46,11 +66,14 @@ object EmbeddedSingleNodeKafkaCluster {
 
   def stop(): Unit = {
     try {
-      broker.stop()
-      zookeeper.stop()
+      broker.foreach(_.stop())
+      zookeeper.foreach(_.stop())
+
     } catch {
-      case t: Throwable =>
+      case t: Throwable => ()
     }
+    broker = None
+    zookeeper = None
   }
 
   /**
@@ -59,44 +82,25 @@ object EmbeddedSingleNodeKafkaCluster {
    *
    * You can use this to e.g. tell Kafka brokers how to connect to this instance.
    */
-  def zKConnectString: String = zookeeper.getConnectString
+  def zKConnectString: String = zookeeper.map(_.getConnectString).getOrElse("")
 
   /**
    * This cluster's `bootstrap.servers` value.  Example: `127.0.0.1:9092`.
    *
    * You can use this to tell Kafka producers how to connect to this cluster.
    */
-  def bootstrapServers: String = broker.brokerList
-
-  /**
-   * Create a Kafka topic with 1 partition and a replication factor of 1.
-   *
-   * @param topic The name of the topic.
-   */
-  def createTopic(topic: String): Unit = {
-    createTopic(topic, 1, 1, new Properties)
-  }
+  def bootstrapServers: String = broker.map(_.brokerList).getOrElse("")
 
   /**
    * Create a Kafka topic with the given parameters.
    *
    * @param topic       The name of the topic.
-   * @param partitions  The number of partitions for this topic.
-   * @param replication The replication factor for (the partitions of) this topic.
+   * @param partitions  The number of partitions for this topic. Default (1)
+   * @param replication The replication factor for (partitions of) this topic. Default (1)
+   * @param topicConfig Additional topic-level configuration settings. Default (Empty properties)
    */
-  def createTopic(topic: String, partitions: Int, replication: Int): Unit = {
-    createTopic(topic, partitions, replication, new Properties)
+  def createTopic(topic: String, partitions: Int = 1, replication: Int = 1, topicConfig: Properties = new Properties): Unit = {
+    broker.foreach(_.createTopic(topic, partitions, replication, topicConfig))
   }
 
-  /**
-   * Create a Kafka topic with the given parameters.
-   *
-   * @param topic       The name of the topic.
-   * @param partitions  The number of partitions for this topic.
-   * @param replication The replication factor for (partitions of) this topic.
-   * @param topicConfig Additional topic-level configuration settings.
-   */
-  def createTopic(topic: String, partitions: Int, replication: Int, topicConfig: Properties): Unit = {
-    broker.createTopic(topic, partitions, replication, topicConfig)
-  }
 }
