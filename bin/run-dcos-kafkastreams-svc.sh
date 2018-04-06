fa47c22648803df1993334ea76ae87d13a0e0060
Gerard <gerard.maas@lightbend.com>
2018-04-06 20:37:24 +0200

    fix template gen

--- bin/run-dcos-kafkastreams-svc.sh
+++ bin/run-dcos-kafkastreams-svc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
-$DIR/run-dcos-svc.sh kafkastreamssvc.json.template
+$DIR/run-dcos-svc.sh kafkastreamssvc.json.template --grafana --influxdb
--- bin/run-dcos-svc.sh
+++ bin/run-dcos-svc.sh
@@ -5,9 +5,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
 source $DIR/functions.sh
 
 JSON_TEMPLATE=$1
+shift
 JSON=${JSON_TEMPLATE%.template}
 APP_NAME=$(cat $JSON_TEMPLATE | jq -r .id)
 APP_STATUS=$(marathon_app_status $APP_NAME)
+
+while test $# != 0
+do
+    case "$1" in
+    --grafana) use_grafana=true ;;
+    --influxdb) use_influxdb=true ;;
+    esac
+    shift
+done
 
 echo "Current App Deployment Status: $APP_STATUS"
 
@@ -21,8 +31,11 @@ else
   cp "$JSON_TEMPLATE" "$JSON"
   update_kafka "$JSON"
   update_zk "$JSON"
-  update_host_port_config "/grafana" "GRAFANA" "$JSON"
-  update_host_port_config "/influxdb" "INFLUXDB" "$JSON"
-
-  dcos marathon app add publisher.json
+  if [ "$use_grafana" = true ]; then
+    update_host_port_config "/grafana" "GRAFANA" "$JSON"
+  fi
+  if [ "$use_influxdb" = true ]; then  
+    update_host_port_config "/influxdb" "INFLUXDB" "$JSON"
+  fi
+  dcos marathon app add $JSON
 fi  
