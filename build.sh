9ac66903ad036bdef89f62c0d9b73028c3119025
Dean Wampler <dean@concurrentthought.com>
2018-05-30 17:22:40 -0700

    Now handles building the docker image(s) like the other sample apps

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -eux
+
+HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+
+echo $HERE
+
+. $HERE/version.sh
+
+cd $HERE
+sbt "set version in ThisBuild := \"$VERSION\"" "show version" clean package docker
