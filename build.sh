7cffe5ad71cdb58d5d124fdc6ae9800f54b629bd
Dean Wampler <dean@concurrentthought.com>
2018-07-11 17:56:26 -0500

    Added an option that prints the docker image names and does nothing else. Needed for the global fdp-release

--- build.sh
+++ build.sh
@@ -1,23 +1,46 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eu
 
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
 
-echo ${HERE}
-
 . "$HERE/version.sh"
 
-# The only allowed argument is the optional version string
+root_dirs=( ${HERE}/bigdl ${HERE}/nwintrusion ${HERE}/flink ${HERE}/kstream )
+
+# The only allowed arguments are the optional version string (no flag) and the
+# flag to just print the Docker image names.
 # Note that because VERSION is exported in version.sh, its value will be propagated
 # to the subsequent build.sh script invocations.
-[[ $# -gt 0 ]] && VERSION=$1
+
+while [[ $# -gt 0 ]]
+do
+  case $1 in
+    -h|--h*)
+      echo "usage: $0 [-h|--help] [--print-docker-image] [VERSION]"
+      exit 0
+      ;;
+    --print-docker-image*)
+      for d in ${root_dirs[@]}
+      do
+        ( cd "$d/source/core"; sbt -no-colors "set version in ThisBuild := \"$VERSION\"" "show docker::imageNames" ) |  grep -e 'lightbend/[^)]*' --only-matching
+      done
+      exit $?
+      ;;
+    -*)
+      echo "ERROR: $0: Unrecognized argument $1"
+      exit 1
+      ;;
+    *)
+     VERSION=$1
+     ;;
+  esac
+  shift
+done
+
 echo "$0: Using version $VERSION"
 
-${HERE}/bigdl/build.sh
-
-${HERE}/nwintrusion/build.sh
-
-${HERE}/flink/build.sh
-
-${HERE}/kstream/build.sh
+for d in ${root_dirs[@]}
+do
+  "$d/build.sh"
+done
