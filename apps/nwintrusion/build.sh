8eac6613e0630a68f821d01856b91f34e28ff3b5
Dean Wampler <dean@concurrentthought.com>
2019-02-05 17:17:37 -0600

    Added the required K8S_OR_DCOS property flag

--- apps/nwintrusion/build.sh
+++ apps/nwintrusion/build.sh
@@ -20,7 +20,7 @@ echo "nwintrusion: VERSION = $VERSION"
 cd ${HERE}/source/core
 for i in fdp-nwintrusion-ingestion fdp-nwintrusion-anomaly fdp-nwintrusion-batchkmeans
 do
-  $NOOP sbt -no-colors "set version in ThisBuild := \"$VERSION\"" "show version" $i/clean $i/$docker_task
+  $NOOP sbt -DK8S_OR_DCOS=K8S -no-colors "set version in ThisBuild := \"$VERSION\"" "show version" $i/clean $i/$docker_task
 done
 
 echo "$PWD: built package and Docker images. $push_msg"
