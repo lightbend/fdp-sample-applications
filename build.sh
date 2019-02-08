02c97144733d5a55c065e3da25860c99a7612f3e
Dean Wampler <dean@concurrentthought.com>
2019-02-07 18:29:20 -0600

    Added a process to run Whitesource on the docker images.

--- build.sh
+++ build.sh
@@ -1,51 +1,132 @@
 #!/usr/bin/env bash
 
 set -eu
+: ${NOOP:=}
 
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
 
 . "$HERE/version.sh"
 
-root_dirs=( ${HERE}/apps/bigdl ${HERE}/apps/nwintrusion ${HERE}/apps/flink ${HERE}/apps/kstream ${HERE}/apps/killrweather ${HERE}/apps/akka-kafka-streams-model-server)
+function help {
+  cat <<EOF
+  $0: Build all sample apps.
+  usage: $0 [-h|--help] [-p|--push-docker-images] [--print|--print-docker-images] [-w|--whitesource] [-v|--version VERSION] [app1 ...]
+  where:
+  -h | --help                 Show this help and exit.
+  -p | --push-docker-images   Do the regular build, including Docker images, then push the images
+                              to Docker Hub. Ignored if --print-docker-images is specified.
+                              (default: build, but don't push the images). Ignored if --no-build specified.
+  -w | --whitesource          Also run the Whitesource report. To just run this report, use --whitesource --no-build.
+  --print | --print-docker-images
+                              Only print the names of the Docker images that would be built. Implies --no-build.
+  -v | --version VERSION      Use VERSION. (default: value set in version.sh)
+  app1 ...                    Process just these apps. The names have to match the directories under
+                              "apps" directory, e.g., "killrweather". (default: build all of them)
+
+  Run this script with "NOOP=echo build.sh ..." to have it echo commands, but not run them.
+EOF
+}
+
+function error {
+  echo "ERROR: $0: $@" 1>&2
+  help 1>&2
+  exit 1
+}
+function info {
+  echo "INFO: $0: $@"
+}
 
 # The only allowed arguments are the optional version string (no flag) and the
 # flag to just print the Docker image names.
 # Note that because VERSION is exported in version.sh, its value will be propagated
 # to the subsequent build.sh script invocations.
 print_docker_image_names=false
+whitesource=false
+push_docker_images=
+apps=()
 while [[ $# -gt 0 ]]
 do
   case $1 in
     -h|--h*)
-      echo "usage: $0 [-h|--help] [--print-docker-image] [VERSION]"
+      help
       exit 0
       ;;
-    --print-docker-image*)
+    --print|--print-docker-image*)
       print_docker_image_names=true
       ;;
+    -w|--white*)
+      whitesource=true
+      ;;
+    -p|--push*)
+      push_docker_images=--push-docker-images
+      ;;
+    -v|--version*)
+      shift
+      [[ $# -eq 0 ]] || [[ -z $1 ]] && error "No value specified for the version!"
+      VERSION=$1
+      ;;
     -*)
-      echo "ERROR: $0: Unrecognized argument $1"
-      exit 1
+      error "Unrecognized argument $1"
       ;;
     *)
-     VERSION=$1
-     ;;
+      apps+=( $1 )
   esac
   shift
 done
+
+[[ -n $VERSION ]] || error "Version string can't be empty!"
+
+function list_app_dirs {
+  for app in ${HERE}/apps/*
+  do
+    name=$(basename $app)
+    case $name in
+      sbt-common-settings) continue ;;
+      *) echo $app ;;
+    esac
+  done
+}
+
+function make_app_dirs {
+  for a in $@
+  do
+    dir=${HERE}/apps/$a
+    [[ -d $dir ]] || error "specified app $a doesn't exist in ${HERE}/apps"
+    echo $dir
+  done
+}
+
+if [[ ${#apps[@]} -eq 0 ]]
+then
+  root_dirs=( $(list_app_dirs) )
+else
+  root_dirs=( $(make_app_dirs ${apps[@]}) )
+fi
 
 if $print_docker_image_names
 then
   for d in ${root_dirs[@]}
   do
-    ( cd "$d/source/core"; sbt -no-colors "set version in ThisBuild := \"$VERSION\"" "show docker::imageNames" ) |  grep -e 'lightbend/[^)]*' --only-matching
+    if [[ -z $NOOP ]]
+    then
+      ( cd "$d/source/core"; sbt -no-colors "set version in ThisBuild := \"$VERSION\"" "show docker::imageNames" ) |  grep -e 'lightbend/[^)]*' --only-matching
+    else
+      ( cd "$d/source/core"; $NOOP sbt -no-colors "set version in ThisBuild := \"$VERSION\"" "show docker::imageNames" )
+    fi
   done
   exit $?
 fi
 
-echo "$0: Using version $VERSION"
+info "Using version $VERSION"
 
 for d in ${root_dirs[@]}
 do
-  "$d/build.sh"
+  info "Running: VERSION=$VERSION $d/build.sh $push_docker_images"
+  NOOP=$NOOP VERSION=$VERSION $d/build.sh $push_docker_images || error "Failed building $d"
 done
+
+if $whitesource
+then
+  make -f whitesource.mkfile all || error "Whitesource report generation failed!"
+fi
+
---
+++ whitesource.mkfile
@@ -0,0 +1,31 @@
+SHELL := bash
+whitesource-jar := whitesource-fs-agent.jar
+whitesource-jar-url := https://raw.githubusercontent.com/whitesource/fs-agent-distribution/master/standAlone/${whitesource-jar}
+config-file := .ws.conf
+
+ifndef VERSION
+$(error VERSION is not defined; either define it while invoking make or use run.sh)
+endif
+ifndef WHITESOURCE_API_KEY
+$(error WHITESOURCE_API_KEY is not defined; either define it while invoking make or use run.sh)
+endif
+
+all: clean whitesource
+
+clean:
+	rm -f ${config-file}
+
+help: ${whitesource-jar}
+	@java -jar ${whitesource-jar} -h
+
+whitesource: ${config-file} ${whitesource-jar}
+	@echo java -jar ${whitesource-jar} -apiKey ... -c ${config-file}
+	@java -jar ${whitesource-jar} -apiKey ${WHITESOURCE_API_KEY} \
+		-productVersion ${VERSION} -projectVersion ${VERSION} -c ${config-file}
+
+${config-file}:
+	@echo "ERROR: $@ doesn't exist"
+	@exit 1
+
+${whitesource-jar}:
+	curl -slJO ${whitesource-jar-url}
